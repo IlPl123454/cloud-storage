3ed5999ad280730b0db866fb513a9d88a2f7eeaf
Ilya <ilplenkov@mail.ru>
2025-11-11 22:52:40 +0300

    контроллер

--- cloud-storage/src/main/java/com/plenkov/cloudstorage/controller/HelloController.java
+++ cloud-storage/src/main/java/com/plenkov/cloudstorage/controller/HelloController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping("/hello")
+@RequestMapping("/")
 public class HelloController {
     @GetMapping
     public String hello() {
-        return "redirect:/hello.html";
+        return "forward:/index.html";
     }
 }

a2f7d165b3c2474ed8b8b93609600f30a5fe08f8
Ilya <ilplenkov@mail.ru>
2025-11-17 23:24:01 +0300

    замена порта на 5432

--- cloud-storage/src/main/java/com/plenkov/cloudstorage/controller/AuthController.java
+++ cloud-storage/src/main/java/com/plenkov/cloudstorage/controller/AuthController.java
@@ -3,16 +3,15 @@ package com.plenkov.cloudstorage.controller;
 import com.plenkov.cloudstorage.dto.auth.UserRegisterRequestDto;
 import com.plenkov.cloudstorage.dto.auth.UserRegisterResponseDto;
 import com.plenkov.cloudstorage.dto.auth.UserSignInRequestDto;
-import com.plenkov.cloudstorage.dto.auth.UserSignInResponseDto;
+import com.plenkov.cloudstorage.dto.auth.UserDto;
 import com.plenkov.cloudstorage.service.AuthService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -20,16 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
+    @Tag(name = "Название контроллера", description = "Описание контроллера")
     @PostMapping("/sign-up")
+    @ResponseStatus(value = HttpStatus.CREATED)
     public UserRegisterResponseDto doRegister(@Valid @RequestBody UserRegisterRequestDto dto) {
         return authService.register(dto);
     }
 
     @PostMapping("/sign-in")
-    public UserSignInResponseDto doSignIn(@Valid @RequestBody UserSignInRequestDto dto,
-                                          HttpServletRequest request,
-                                          HttpServletResponse response) {
-
+    public UserDto doSignIn(@Valid @RequestBody UserSignInRequestDto dto,
+                            HttpServletRequest request,
+                            HttpServletResponse response) {
         return authService.authenticate(dto, request, response);
     }
 }
